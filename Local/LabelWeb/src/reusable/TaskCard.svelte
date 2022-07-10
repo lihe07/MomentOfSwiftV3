b52f329d5e29285f25ba8f696719613fb211a0f3
lihe07 <li@imlihe.com>
2022-07-10 21:16:16 +0800

    更新

--- Local/LabelWeb/src/reusable/TaskCard.svelte
+++ Local/LabelWeb/src/reusable/TaskCard.svelte
@@ -1,3 +1,11 @@
+<script>
+    export let thumbnail_url = "https://raw.githubusercontent.com/lihe07/MomentOfSwiftV3/main/Local/SSOWeb/src/791531a78c9416e709ff627dfc1fbe4.jpg";
+    export let num = 123;
+    export let creator_id = "";
+    export let creator_name = "lihe07";
+    export let creator_avatar = "https://avatars.githubusercontent.com/u/53819558?v=4";
+</script>
+
 <div class="task-card">
     taskcard
 </div>
--- Local/LabelWeb/src/routes/MyTasks.svelte
+++ Local/LabelWeb/src/routes/MyTasks.svelte
@@ -2,8 +2,26 @@
     // Stores & APIs
     import {theme} from "../stores";
     import {fade} from "svelte/transition";
+    // Reusable
+    import Pagination from "../reusable/Pagination.svelte";
+    import Card from "../reusable/Card.svelte";
+    import TaskCard from "../reusable/TaskCard.svelte";
 </script>
 
 <div in:fade class:dark={$theme === "dark"} class:light={$theme === "light"}>
-    My Tasks
+    <div class="list">
+        <Card>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 12 12">
+                    <g fill="none">
+                        <path d="M6.5 1.75a.75.75 0 0 0-1.5 0V5H1.75a.75.75 0 0 0 0 1.5H5v3.25a.75.75 0 0 0 1.5 0V6.5h3.25a.75.75 0 0 0 0-1.5H6.5V1.75z"
+                              fill="currentColor" style="--darkreader-inline-fill: currentColor;"
+                              data-darkreader-inline-fill=""></path>
+                    </g>
+                </svg>
+                <h2>创建新计数任务</h2>
+            </div>
+        </Card>
+    </div>
+    <Pagination/>
 </div>
